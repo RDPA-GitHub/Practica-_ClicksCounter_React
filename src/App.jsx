0c66a8f322c4ad46e598b8f129e20d02ab6e0e05
Ronald Pe#a <ronald.micros@gmail.com>
2023-03-09 20:59:46 -0300

    FCC - Clics Counter (Template)

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css';
+import RP from './img/Logo.png';
+
+const App = () => {
+
+  return (
+    <div className="App">
+      <div className="Rp-logo-container"> 
+        <img
+          className='Rp-logo'
+          src={RP}
+          alt='Logo de Rp ' />
+      </div>
+      
+      <div className="contenedor-principal">
+
+      </div>
+    </div>
+  )
+}
+
+export default App;
